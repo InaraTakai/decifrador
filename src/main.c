9654bb111d60bc516dace43a0250cc03ee082dfe
Inara <inaratakashi@gmail.com>
2020-08-12 01:56:21 -0300

    Lab funcionando

--- src/main.c
+++ src/main.c
@@ -6,15 +6,39 @@
 
 #include <stdio.h>
 
-#define TAM_BUFFER 50
+#define TAM_BUFFER 60
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
 
+const char senha[] = "SENHASECRETA";
+
+char decriptacao(char c, char k) {
+  char c0, k0, p;
+  if (c>='A' && c<='Z') {
+    c0 = c - 'A';
+    k0 = k - 'A';
+    p = 'A' + (c0 - k0 + ('Z'-'A'+1)) % ('Z'-'A'+1);
+    return p;
+  } else {
+    return c;
+  }
+}
+
+
 int main() {
+  int j=0;
+  int i=0;
   /* Leitura da entrada */
   fgets(buffer_entrada, TAM_BUFFER, stdin);
 
-
+  for (i=0; (i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n'); i++) {
+    buffer_saida[i] = decriptacao(buffer_entrada[i], senha[j]);
+    j++;
+    if (senha[j]=='\0'){
+      j=0;
+    }
+  }
+  buffer_saida[i]='\0';
 
   /* Escrever saida na tela */
   printf("%s\n", buffer_saida);
